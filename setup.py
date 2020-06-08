77a9c973a569f1884bcad80eebf916ba8f3ad9e9
Tanya <twest1087@gmail.com>
2020-06-08 21:09:37 +0700

    added docstirings and removed commented code

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(name='opencart-tests',
+      version='1.0',
+      url='https://github.com/twesttt/dz6-selenium-selectors',
+      license='MIT',
+      author='Tatyana Trofimenko',
+      author_email='twest1087@gmail.com',
+      description='Tests for opencart',
+      keywords='otus, opencart, tests',
+      packages=find_packages(),
+      long_description=open('README.md').read(),
+      classifiers=[
+          "License :: OSI Approved :: Python Software Foundation License"
+      ]
+      )
--- tests/test_remote_machine_services.py
+++ tests/test_remote_machine_services.py
@@ -5,8 +5,9 @@ from requests.exceptions import ConnectionError
 from helpers.connect_to_db import connect_db
 
 
-
 def check_opencart_status():
+    """Check Opencart response status: 0 if it doesn't reachable, 1 if status code is 200"""
+
     attempts = 1
     status = 1
     while attempts < 10:
@@ -28,6 +29,8 @@ def check_opencart_status():
 
 
 def test_opencart_server_restart(connect_ssh):
+    """First apache is stopped and checked it is not running any more, that we start it and check it response"""
+
     connect_ssh.exec_command("echo 111 | sudo -S service apache2 stop")
     opencart_status = check_opencart_status()
     assert opencart_status == 0
@@ -38,6 +41,8 @@ def test_opencart_server_restart(connect_ssh):
 
 
 def check_db_using_db_connect():
+    """Connect to MySQL database: returns 0 if not successful and 1 if connection is done"""
+
     timeout = time.time() + 30
     while True:
         time.sleep(2)
@@ -48,6 +53,7 @@ def check_db_using_db_connect():
 
 
 def test_db_status(connect_ssh):
+    """First MySQL server is stopped, then check it is stopped, than we start the service and check it is up"""
     connect_ssh.exec_command("echo 111 | sudo -S service mysql stop")
     db_status = check_db_using_db_connect()
     assert db_status == 0
@@ -55,26 +61,3 @@ def test_db_status(connect_ssh):
     db_status = check_db_using_db_connect()
     assert db_status != 0
 
-
-# def check_db_using_socket():
-#     timeout = time.time() + 15
-#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#     while True:
-#         time.sleep(2)
-#         if sock.connect_ex(('192.168.0.101', 3306)) != 0:
-#             status = 0
-#         else:
-#             status = 1
-#         if time.time() > timeout:
-#             break
-#     sock.close()
-#     return status
-
-
-# def test_mysql_server_restart(connect_ssh):
-#     connect_ssh.exec_command("echo 111 | sudo -S service mysql stop")
-#     db_status = check_db_using_socket()
-#     assert db_status == 0
-#     connect_ssh.exec_command("echo 111 | sudo -S service mysql start")
-#     db_status = check_db_using_socket()
-#     assert db_status == 1
